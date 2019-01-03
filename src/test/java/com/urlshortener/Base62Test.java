e2d33ed2c0b14c071e3659b4fcc0ea91fe38374e
leeyunjea <yunjea0312@naver.com>
2019-01-03 19:51:45 +0900

    Update url-shortener
    update url-shortner

--- src/test/java/com/urlshortener/Base62Test.java
+++ src/test/java/com/urlshortener/Base62Test.java
@@ -13,6 +13,10 @@ public class Base62Test {
 		//System.out.println(base62.encode(str.hashCode()));
 		System.out.println(base62.encode(0L));
 		
+		long id = (long) (Math.random() * (99999 - 10000 + 1)) + 10000;
+
+		String shortUrl = base62.encode(id);
+		System.out.println(shortUrl);
 		
 	}
 	
